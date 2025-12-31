19ccf2d6baf5ceea1bec5a6c1e7fab3c1e9d3a18
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-12-31 20:30:07 +0530

    Refactor ChatService

--- src/main/java/com/myorganisation/OwnGpt/service/ChatService.java
+++ src/main/java/com/myorganisation/OwnGpt/service/ChatService.java
@@ -1,36 +1,10 @@
 package com.myorganisation.OwnGpt.service;
 
+import com.myorganisation.OwnGpt.dto.request.ChatRequestDto;
+import com.myorganisation.OwnGpt.dto.response.ChatResponseDto;
+import com.myorganisation.OwnGpt.dto.response.LlmListResponseDto;
 
-import org.springframework.ai.chat.model.ChatModel;
-import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.ollama.api.OllamaChatOptions;
-import org.springframework.stereotype.Service;
-
-@Service
-public class ChatService {
-    private final ChatModel chatModel;
-
-    public ChatService(ChatModel chatModel) {
-        this.chatModel = chatModel;
-    }
-    public String test() {
-        ChatResponse response = chatModel.call(
-                new Prompt(
-                        "Calculate 17 × 23",
-                        OllamaChatOptions.builder()
-                                .model("deepseek-r1:1.5b")
-                                .temperature(0D)
-                                .enableThinking()
-                                .build()
-                ));
-
-        String thinking = response.getResult().getMetadata().get("thinking");
-        System.out.println("Reasoning: " + thinking);
-
-        String answer = response.getResult().getOutput().getText();
-        System.out.println("Answer: " + answer);
-
-        return "Working";
-    }
+public interface ChatService {
+    LlmListResponseDto getLlmList();
+    ChatResponseDto chatPrompt(ChatRequestDto chatRequestDto);
 }
